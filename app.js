39593342c6ba0ea047a8b22fed35669ffa826a95
ys-code-box <Srini.Yarramsetty@gmail.com>
2022-04-27 22:23:43 +0530

    Changed Message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CD / CI Pipeline Testing Sample - by Srini !'));
+app.get('/', (req, res) => res.send('CD / CI Pipeline Testing Sample - By Srini !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
