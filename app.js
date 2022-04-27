1c948fe818c3ad295a8ad61e457260c80b3256e7
unknown <Srini.Yarramsetty@gmail.com>
2022-04-27 21:42:47 +0530

    changed res message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('CD / CI Pipeline Testing Sample - by Srini !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
